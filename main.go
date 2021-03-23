640cd652dbeec9ccfb8afc55a8cdb11b6910c7e4
xxxsen <xxxsen@gmail.com>
2021-03-23 20:18:39 +0800

    feat: ui

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"retroeditor/parser"
+	"os"
+	"retroeditor/ui"
+
+	"github.com/therecipe/qt/widgets"
 )
 
 func main() {
-	p := parser.NewGameListParser("/home/sen/tmp/gamelist.xml")
-	err := p.Parse()
-	p.Remove("Ball Breakers")
-	p.Save()
-	if err != nil {
-		panic(err)
-	}
-	// widgets.NewQApplication(len(os.Args), os.Args)
-	// window := ui.NewRetroUI()
-	// window.Show()
-	// widgets.QApplication_Exec()
+	widgets.NewQApplication(len(os.Args), os.Args)
+	window := ui.NewRetroUI()
+	window.Show()
+	widgets.QApplication_Exec()
 }
